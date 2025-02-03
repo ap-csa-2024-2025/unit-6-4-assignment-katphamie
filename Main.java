feb71163d7487b8ba5941b859b5241f5794d92e0
katphamie <katnphmie@gmail.com>
2025-02-03 19:20:18 +0000

    hai

--- Main.java
+++ Main.java
@@ -10,19 +10,44 @@ public class Main
 
   public static int countLength(String[] arr, int targetLength)
   {
-    // replace with your code
+    int counter = 0;
+    for (i = 0; i < arr.length; i++)
+    {
+      if (arr[i].length == targetLength)
+      {
+        counter++;
+      }
+    }
     return 0;
   }
 
   public static int indexOf(double[] arr, double target)
   {
-    // replace with your code
+    int i = 0;
+    while (i < arr.length)
+    {
+      if (arr[i].length == target)
+      {
+        return i;
+      }
+      i++;
+    }
     return -1;
   }
 
   public static boolean hasDuplicates(int[] arr)
   {
-    // replace with your code
+    for (i=0; i < arr.length; i++)
+    {
+      int elem = arr[i];
+      for (j = i; j < arr.length; j++)
+      {
+        if (arr[j] == elem)
+        {
+          return true;
+        }
+      }
+    }
     return false;
   }
 
